2bc7e4de3952550d5c442d546d5e9de57e0fdd0f
Nicolas Dupouy <nicolas.dupouy17@orange.fr>
2025-03-24 17:21:21 +0100

    Intégration de la page de login admin et ajout du code pour que l'admin puisse se connecter

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,13 @@
+let works = [];
+let categories = [];
+const portfolio = document.getElementById("portfolio");
+const categoryFilters = document.createElement("div")
+categoryFilters.classList.add("category-filters")
+portfolio.appendChild(categoryFilters)
+const gallery = document.createElement("div");
+gallery.classList.add("gallery");
+portfolio.appendChild(gallery)
+
 async function getCategories() {
     const reponse = await fetch ("http://localhost:5678/api/categories");
     const categories = await reponse.json();
@@ -8,30 +18,24 @@ getCategories();
 
 async function getWorks() {
     const reponse = await fetch ("http://localhost:5678/api/works");
-    const works = await reponse.json();
+    works = await reponse.json();
     console.log(works)
     showWorks(works);
 }
 getWorks();
 
-function showWorks(worksArray) {
-
-    const portfolio = document.getElementById("portfolio");
-    const gallery = document.createElement("div");
-        gallery.classList.add("gallery");
-
-    portfolio.appendChild(gallery)
+function showWorks(data) {
 
     gallery.innerHTML = ""
 
-    for (let i = 0; i < worksArray.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         const figure = document.createElement("figure")
         const image = document.createElement("img")
         const figcaption = document.createElement("figcaption")
 
-        image.src = worksArray[i].imageUrl
-        image.alt = worksArray[i].title
-        figcaption.innerText = worksArray[i].title
+        image.src = data[i].imageUrl
+        image.alt = data[i].title
+        figcaption.innerText = data[i].title
 
         gallery.appendChild(figure)
         figure.appendChild(image)
@@ -40,10 +44,6 @@ function showWorks(worksArray) {
 }
 
 function showCategoryFilters (categoryArray) {
-
-    const categoryFilters = document.createElement("div")
-    categoryFilters.classList.add(".category-filters")
-    portfolio.appendChild(categoryFilters)
 
     const buttonTous = document.createElement("button")
         buttonTous.classList.add("btn-filtres","btn-filtres-full")
@@ -54,6 +54,7 @@ function showCategoryFilters (categoryArray) {
     buttonTous.addEventListener("click", () => {
         clearBtnFull()
         buttonTous.classList.add("btn-filtres-full")
+        showWorks(works)
     })
 
     for (let i = 0; i < categoryArray.length; i++) {
@@ -83,3 +84,4 @@ function clearBtnFull() {
     });
 }
 
+/* PARTIE MODALE */
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,28 @@
-async function getLogin() {
-    const reponse = await fetch("http://localhost:5678/api/users/login");
-    const login = await reponse.json();
-    console.log(works);
+loginConnect()
+
+async function loginConnect() {
+    const loginForm = document.querySelector(".login-form")
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+
+        const fetchLogin = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({email, password})
+        })
+
+        if(fetchLogin.ok) {
+            const dataApi = await fetchLogin.json()
+            sessionStorage.setItem("token", dataApi.token)
+            window.location.href = "index.html"
+        }
+        
+        else {
+            alert("Connexion échouée")
+        }
+    })
 }
-getLogin()
